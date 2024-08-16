9d8536300c9a25e05c6e3b3d0733a8daa0bdb9c2
Locez <loki.a@live.cn>
2024-08-16 23:48:14 +0800

    feat: Init

--- README.md
+++ README.md
@@ -2,24 +2,26 @@
 
 A crate to convert bdf font to sdl2 surface
 
- #  Example
+# Example
 
-``` rust
+```rust
 use sdl2::surface::Surface;
 use bdf2surface::{Color, Converter, Text};
-let sdl_context = sdl2::init()?;
-       let _video_subsystem = sdl_context.video()?;
-       let path = "./resource/wqy_9pt.bdf";
-       let converter = Converter::new(path);
 
-       let texts = vec![
-           Text("你好，露西！".to_string(), Color::RGB(0, 0, 0)),
-           Text(
-               "Hello,                    ".to_string(),
-               Color::RGB(255, 0, 0),
-           ),
-           Text("Locez!".to_string(), Color::RGB(255, 73, 170)),
-       ];
-       let surface = converter.render(&texts, 60).unwrap();
-       surface.save_bmp("output.bmp")?;
+
+let sdl_context = sdl2::init()?;
+let _video_subsystem = sdl_context.video()?;
+let path = "./resource/wqy_9pt.bdf";
+let converter = Converter::new(path);
+
+let texts = vec![
+    Text("你好，露西！".to_string(), Color::RGB(0, 0, 0)),
+    Text(
+        "Hello,                    ".to_string(),
+        Color::RGB(255, 0, 0),
+    ),
+    Text("Locez!".to_string(), Color::RGB(255, 73, 170)),
+];
+let surface = converter.render(&texts, 60).unwrap();
+surface.save_bmp("output.bmp")?;
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -8,21 +8,23 @@ use sdl2::surface::Surface;
 /// ```
 /// use sdl2::surface::Surface;
 /// use bdf2surface::{Color, Converter, Text};
-/// let sdl_context = sdl2::init()?;
-///        let _video_subsystem = sdl_context.video()?;
-///        let path = "./resource/wqy_9pt.bdf";
-///        let converter = Converter::new(path);
 ///
-///        let texts = vec![
-///            Text("你好，露西！".to_string(), Color::RGB(0, 0, 0)),
-///            Text(
-///                "Hello,                    ".to_string(),
-///                Color::RGB(255, 0, 0),
-///            ),
-///            Text("Locez!".to_string(), Color::RGB(255, 73, 170)),
-///        ];
-///        let surface = converter.render(&texts, 60).unwrap();
-///        surface.save_bmp("output.bmp")?;
+///
+/// let sdl_context = sdl2::init()?;
+/// let _video_subsystem = sdl_context.video()?;
+/// let path = "./resource/wqy_9pt.bdf";
+/// let converter = Converter::new(path);
+///
+/// let texts = vec![
+///     Text("你好，露西！".to_string(), Color::RGB(0, 0, 0)),
+///     Text(
+///         "Hello,                    ".to_string(),
+///         Color::RGB(255, 0, 0),
+///     ),
+///     Text("Locez!".to_string(), Color::RGB(255, 73, 170)),
+/// ];
+/// let surface = converter.render(&texts, 60).unwrap();
+/// surface.save_bmp("output.bmp")?;
 /// ```
 ///
 
